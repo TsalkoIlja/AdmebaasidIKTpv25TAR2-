dd02670ca4e9e3304e22fc8915cf7ffe987adea7
TsalkoIlja <iljatsalko9@gmail.com>
2026-03-05 10:21:43 +0200

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,102 +1,78 @@
-CREATE DATABASE veebipoodTsalko;
-USE veebipoodTsalko;
---tabel Categories
-CREATE TABLE categories(
-categories_id int PRIMARY KEY identity(1,1),
-category_name varchar(30) unique);
+CREATE DATABASE trennidIT;
+USE trennidIT;
 
-INSERT INTO categories(category_name)
-VALUES ('T-särk'), ('mantel'), ('pusa'), ('pintsak');
-SELECT * FROM categories;
---tabel products
-CREATE TABLE products(
-product_id int  PRIMARY KEY identity(1,1),
-product_name varchar(50) NOT NULL,
-brand_id int,
-category_id int,
-FOREIGN KEY (category_id) references categories(categories_id),
-model_year int,
-list_price decimal (7,2));
---tabel brands
-
-
-CREATE TABLE brands (
-brand_id int  PRIMARY KEY identity(1,1),
-brand_name varchar(30) unique);
-
-
-ALTER TABLE ADD CONSTRAINT fk_brand
-FOREIGN KEY (brand_id) references brand(brand_id);
-
---tabel stack -- kaks primary key
-CREATE TABLE stacks(
-store_id int,
-product_id int,
-quantity int,
-PRIMARY KEY (store_id, product_id),
-foreign key (product_id) references products(product_id)
+CREATE TABLE Treener(
+treener_id INT PRIMARY KEY IDENTITY(1,1),
+nimi VARCHAR(50) NOT NULL,
+kontakt VARCHAR(20) UNIQUE NOT NULL
 );
+INSERT INTO Treener (nimi, kontakt)
+VALUES ('Marko Saar', '5551411'),
+('Aneli Saura', '5261566');
 
--- table customers
-CREATE TABLE customers (
-    customer_id INT PRIMARY KEY IDENTITY(1,1),
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    phone CHAR(15),
-    email VARCHAR(50) UNIQUE,
-    street VARCHAR(50),
-    city VARCHAR(50),
-    state VARCHAR(50),
-    zip_code CHAR(5)
+CREATE TABLE Trenn(
+trenn_id INT PRIMARY KEY IDENTITY(1,1),
+nimetus VARCHAR(50) NOT NULL,
+kirjeldus VARCHAR(200),
+toimumisaeg DATETIME NOT NULL,
+koht VARCHAR(50) NOT NULL,
+treener_id INT,
+raskusaste VARCHAR(20) CHECK (raskusaste IN ('Kerge', 'Keskmine', 'Raske')),
+FOREIGN KEY (treener_id) REFERENCES Treener(treener_id) 
 );
+INSERT INTO Trenn (nimetus, kirjeldus, toimumisaeg, koht, raskusaste, treener_id) VALUES
+('Jooga', 'Rahulik treening', '2024', 'Saali A', 'Kerge', 1),
+('Box', 'Tugevus treening', '2025', 'Saali B', 'Keskmine', 1);
 
-FOREIGN KEY (staff_id) REFERENCES staffs(staff_id));
-
-
---tabel orders 
-SELECT * FROM orders;
-
-CREATE TABLE order_items (
-    order_id INT,
-    item_id INT,
-    product_id INT,
-    quantity INT,
-    list_price DECIMAL(7,2),
-    discount INT,
-    PRIMARY KEY (order_id, item_id),
-    FOREIGN KEY (product_id) REFERENCES products(product_id),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+CREATE TABLE Osaleja(
+osaleja_id INT PRIMARY KEY IDENTITY(1,1),
+nimi VARCHAR(50) NOT NULL,
+vanus INT CHECK (vanus>= 5 AND vanus <=100),
+sugu CHAR(1) CHECK (sugu IN('M', 'N')),
+telefon VARCHAR(20) UNIQUE 
 );
+INSERT INTO Osaleja (nimi, vanus, sugu, telefon) VALUES
+('Maria Ivanova', 25, 'N', '5550001'),
+('Ilja Tsalko', 30, 'M', '5540011');
 
-ALTER TABLE stocks
-ADD CONSTRAINT fk_store
-FOREIGN KEY (store_id) REFERENCES stores(store_id);
+CREATE TABLE Registreerimine(
+reg_id INT PRIMARY KEY IDENTITY(1,1),
+trenn_id INT NOT NULL,
+osaleja_id INT NOT NULL,
+kuupäev DATE,
+staatus VARCHAR(20) CHECK (staatus IN ('Kinnitatud', 'Ootel', 'Tühistatud')),
+FOREIGN KEY (trenn_id) REFERENCES Trenn(trenn_id),
+FOREIGN KEY (osaleja_id) REFERENCES Osaleja(osaleja_id)
+);
+INSERT INTO Registreerimine (trenn_id, osaleja_id, staatus) VALUES
+(2, 25, 'Kinnitatud'),
+(4, 30, 'Ootel');
 
-SELECT * FROM order_items;
+CREATE PROCEDURE lisaOsaleja
+@nimi VARCHAR(50),
+@vanus INT,
+@sugu CHAR(1),
+@telefon VARCHAR(20)
+AS
+BEGIN
+INSERT INTO Osaleja (nimi, vanus, sugu, telefon)
+VALUES (@nimi), @vanus, @sugu, @telefon);
 
+SELECT * From Osaleja;
+END;
+GO
 
+CREATE PROCEDURE otsiTrenn
+@raskus VARCHAR(20)
+AS
+BEGIN
+SELECT * FROM Trenn
+WHERE raskusaste = @raskus;
+END;
+GO
 
-
-
-
-
-
-
---DROP TABLE stacks;
-
-
-
-
-
---INSERT into brands
-INSERT into products (
-product_name, brand_id, category_id, model_year, list_price)
-VALUES
-('logoga T-särk',..., 1, 2020, 25,50);
-
-
-SELECT * FROM brands;
-SELECT * FROM categories;
-SELECT * FROM products;
-SELECT * FROM stocks;
+SELECT * FROM Osaleja;
+SELECT * FROM Registreerimine;
+SELECT * FROM Treener;
+SELECT * FROM Trenn;
 
