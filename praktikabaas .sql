1d2dbeca4a932589fed5d862f004b458a3337e7d
TsalkoIlja <iljatsalko9@gmail.com>
2026-04-01 09:53:23 +0300

    Refactor SQL script for database creation and procedures

--- praktikabaas .sql
+++ praktikabaas .sql
@@ -29,7 +29,7 @@ INSERT INTO praktikajuhendaja (eesnimi, perekonnanimi, synniaeg, telefon) VALUES
 ('Katrin', 'Kask', '1990-03-22', '55544422'),
 ('Andres', 'Mets', '1978-11-05', '55544433'),
 ('Liisa', 'Põld', '1988-07-19', '55544444'),
-('Ilja', 'Vaher', '2009-10-30', '55544455');
+('Ilja', 'Tsalko', '2009-10-30', '55544455');
 
 Select * From praktikajuhendaja;
 
@@ -133,8 +133,10 @@ FROM praktikajuhendaja
 WHERE MONTH(synniaeg) IN (9, 10, 11);
 
 --Kontroll:
-SELECT * FROM vw_firma_praktikakohad;
-SELECT * FROM vw_sugisel_sundinud;
+SELECT * FROM vw_firma_praktikakohad
+WHERE kogus = 1;
+SELECT * FROM vw_sugisel_sundinud
+Where eesnimi Like 'i%' ;
 
 --6. Protseduurid
 --6.1 Protseduur: lisa uus firma
@@ -154,33 +156,30 @@ GO
 
 Select * From firma;
 
---Protseduur: lisa uus veerg tabelisse praktikajuhendaja
---SQL Server lubab ALTER TABLE otse protseduuris — seega see töötab:
-DROP PROCEDURE IF EXISTS lisaVeergPalk;
-GO
-CREATE PROCEDURE lisaVeergPalk
+
+--muudatus
+
+CREATE PROCEDURE muudatus
+@tegevus varchar(10),
+@tabelinimi varchar(25),
+@veerunimi varchar(25),
+@tyyp varchar(25) =null
 AS
 BEGIN
-    IF NOT EXISTS (
-        SELECT *
-        FROM INFORMATION_SCHEMA.COLUMNS
-        WHERE TABLE_NAME = 'praktikajuhendaja'
-          AND COLUMN_NAME = 'palk'
-    )
-    BEGIN
-        ALTER TABLE praktikajuhendaja
-        ADD palk DECIMAL(8,2);
-        PRINT 'Veerg palk lisatud.';
-    END
-    ELSE
-    BEGIN
-        PRINT 'Veerg palk on juba olemas.';
-    END
+DECLARE @sqltegevus as varchar(max)
+set @sqltegevus=case 
+when @tegevus='add' then concat('ALTER TABLE ', 
+@tabelinimi, ' ADD ', @veerunimi, ' ', @tyyp)
+when @tegevus='drop' then concat('ALTER TABLE ', 
+@tabelinimi, ' DROP COLUMN ', @veerunimi)
 END;
-GO
+print @sqltegevus;
+begin 
+EXEC (@sqltegevus);
+END
+END;
 
-EXEC lisaVeergPalk;
-GO
+EXEC muudatus @tegevus='add', @tabelinimi='praktikajuhendaja', @veerunimi='test', @tyyp='int';
 
 --Protseduur: juhendajate keskmise palga arvutamine
 CREATE PROCEDURE juhendajateKeskminePalk
@@ -192,3 +191,92 @@ END;
 GO
 EXEC juhendajateKeskminePalk;
 GO
+
+--Lisa funktsioon 1: fnComputeAge (aastad, kuud, päevad)
+CREATE FUNCTION fnComputeAge(@DOB datetime)
+RETURNS nvarchar(50)
+AS 
+BEGIN
+    DECLARE @tempdate datetime, @years int, @months int, @days int;
+
+    SET @tempdate = @DOB;
+
+    -- Aastad
+    SET @years = DATEDIFF(YEAR, @tempdate, GETDATE()) 
+                 - CASE 
+                       WHEN (MONTH(@DOB) > MONTH(GETDATE())) 
+                         OR (MONTH(@DOB) = MONTH(GETDATE()) AND DAY(@DOB) > DAY(GETDATE()))
+                       THEN 1 
+                       ELSE 0 
+                   END;
+
+    SET @tempdate = DATEADD(YEAR, @years, @tempdate);
+
+    -- Kuud
+    SET @months = DATEDIFF(MONTH, @tempdate, GETDATE()) 
+                  - CASE 
+                        WHEN DAY(@DOB) > DAY(GETDATE()) THEN 1 
+                        ELSE 0 
+                    END;
+
+    SET @tempdate = DATEADD(MONTH, @months, @tempdate);
+
+    -- Päevad
+    SET @days = DATEDIFF(DAY, @tempdate, GETDATE());
+
+    DECLARE @Age nvarchar(50);
+    SET @Age = 
+        CAST(@years AS nvarchar(4)) + ' Years ' +
+        CAST(@months AS nvarchar(2)) + ' Months ' +
+        CAST(@days AS nvarchar(2)) + ' Days old';
+
+    RETURN @Age;
+END;
+GO
+
+--Lisa funktsioon 2: CalculateAge (ainult aastad)
+CREATE FUNCTION dbo.CalculateAge(@DOB date)
+RETURNS int
+AS 
+BEGIN
+    DECLARE @Age int;
+
+    SET @Age = DATEDIFF(YEAR, @DOB, GETDATE()) -
+        CASE
+            WHEN (MONTH(@DOB) > MONTH(GETDATE())) 
+              OR (MONTH(@DOB) = MONTH(GETDATE()) AND DAY(@DOB) > DAY(GETDATE()))
+            THEN 1
+            ELSE 0
+        END;
+
+    RETURN @Age;
+END;
+GO
+
+
+---- Kontroll – kas funktsioonid töötavad praktikajuhendaja tabeliga
+--Detailne vanus (aastad, kuud, päevad)
+SELECT 
+    eesnimi,
+    perekonnanimi,
+    synniaeg,
+    dbo.fnComputeAge(synniaeg) AS detailne_vanus
+FROM praktikajuhendaja;
+
+--Vanus ainult aastates
+SELECT 
+    eesnimi,
+    perekonnanimi,
+    synniaeg,
+    dbo.CalculateAge(synniaeg) AS vanus_aastates
+FROM praktikajuhendaja;
+
+
+--
+SELECT 
+    eesnimi,
+    perekonnanimi,
+    synniaeg,
+    dbo.fnComputeAge(synniaeg) AS detailne_vanus,
+    dbo.CalculateAge(synniaeg) AS vanus_aastates
+FROM praktikajuhendaja;
