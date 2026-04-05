360261d715aa0db5e226e9830517f714fdeb0b51
TsalkoIlja <iljatsalko9@gmail.com>
2026-04-05 16:19:05 +0300

    Update SQL schema and data for animal shelter

--- loomad_formatted.sql
+++ loomad_formatted.sql
@@ -1,57 +1,61 @@
-CREATE DATABASE loomadVarjupaik;
-USE loomadVarjupaik;
+CREATE DATABASE loomade_andmebaas;
+USE loomade_andmebaas;
 
--- tabel Boks
+-- Boks tabel
 CREATE TABLE boks(
     boks_id INT PRIMARY KEY identity(1,1),
-    nimetus VARCHAR(30),
+    nimetus VARCHAR(50),
     asukoht VARCHAR(50),
     mahutavus INT
 );
 
--- andmete lisamine boks
 INSERT INTO boks(nimetus, asukoht, mahutavus)
-VALUES ('Boks A', 'Vasak tiib', 3),
-       ('Boks B', 'Parem tiib', 2),
-       ('Boks C', 'Keskmine ala', 4),
-       ('Boks D', 'Tagumine ruum', 1),
-       ('Boks E', 'Uksest paremal', 2);
+VALUES ('Boks A', '1. korrus', 3),
+       ('Boks B', '1. korrus', 2),
+       ('Boks C', '2. korrus', 4),
+       ('Boks D', '2. korrus', 1),
+       ('Boks E', '3. korrus', 5);
 
--- tabel Vabatahtlik
+SELECT * FROM boks;
+
+-- Vabatahtlik tabel
 CREATE TABLE vabatahtlik(
     vabatahtlik_id INT PRIMARY KEY identity(1,1),
     nimi VARCHAR(50),
     kontakt VARCHAR(30)
 );
 
--- vabatahtlikud
 INSERT INTO vabatahtlik(nimi, kontakt)
-VALUES ('Mari', '555-111'),
-       ('Jaan', '555-222'),
-       ('Kertu', '555-333'),
-       ('Mark', '555-444'),
-       ('Anna', '555-555');
+VALUES ('Anna', '555-111'),
+       ('Markus', '555-222'),
+       ('Katrin', '555-333'),
+       ('Ilja', '555-444'),
+       ('Maria', '555-555');
 
--- tabel Loom
+SELECT * FROM vabatahtlik;
+
+-- Loom tabel
 CREATE TABLE loom(
     loom_id INT PRIMARY KEY identity(1,1),
-    liik VARCHAR(30),
-    tõug VARCHAR(30),
-    sugu CHAR(1),
+    nimi VARCHAR(50),
+    liik VARCHAR(50),
+    tõug VARCHAR(50),
+    sugu VARCHAR(10),
     saabumise_kuupäev DATE,
     boks_id INT,
     FOREIGN KEY (boks_id) REFERENCES boks(boks_id)
 );
 
--- loomad
-INSERT INTO loom(liik, tõug, sugu, saabumise_kuupäev, boks_id)
-VALUES ('Koer', 'Labrador', 'M', '2024-01-05', 1),
-       ('Kass', 'Maine Coon', 'N', '2024-01-10', 2),
-       ('Koer', 'Husky', 'M', '2024-01-12', 3),
-       ('Kass', 'Siiam', 'N', '2024-01-15', 4),
-       ('Jänes', 'Valge', 'M', '2024-01-20', 5);
+INSERT INTO loom(nimi, liik, tõug, sugu, saabumise_kuupäev, boks_id)
+VALUES ('Muri', 'Koer', 'Labrador', 'Isane', '2024-01-10', 1),
+       ('Miisu', 'Kass', 'Siiam', 'Emane', '2024-01-12', 2),
+       ('Rex', 'Koer', 'Husky', 'Isane', '2024-01-15', 3),
+       ('Bella', 'Kass', 'Maine Coon', 'Emane', '2024-01-18', 4),
+       ('Poku', 'Jänes', 'Valge', 'Isane', '2024-01-20', 5);
 
--- tabel Koristus
+SELECT * FROM loom;
+
+-- Koristus tabel
 CREATE TABLE koristus(
     koristus_id INT PRIMARY KEY identity(1,1),
     loom_id INT,
@@ -62,73 +66,59 @@ CREATE TABLE koristus(
     FOREIGN KEY (vabatahtlik_id) REFERENCES vabatahtlik(vabatahtlik_id)
 );
 
--- koristused
 INSERT INTO koristus(loom_id, vabatahtlik_id, kuupäev, kestus)
 VALUES (1, 1, '2024-02-01', 30),
        (2, 2, '2024-02-02', 25),
        (3, 3, '2024-02-03', 40),
        (4, 4, '2024-02-04', 20),
-       (5, 5, '2024-02-05', 15);
+       (5, 5, '2024-02-05', 35);
 
--- minu lisatabel: Toit
-CREATE TABLE toit(
-    toit_id INT PRIMARY KEY identity(1,1),
-    nimetus VARCHAR(50),
-    kogus INT,
-    loom_id INT,
-    FOREIGN KEY (loom_id) REFERENCES loom(loom_id)
-);
-
-
--- toit
-INSERT INTO toit(nimetus, kogus, loom_id)
-VALUES ('Koeratoit', 5, 1),
-       ('Kassitoit', 3, 2),
-       ('Koeratoit', 4, 3),
-       ('Kassitoit', 2, 4),
-       ('Jänesetoit', 1, 5);
-
-SELECT * FROM loom;
-SELECT * FROM boks;
-SELECT * FROM vabatahtlik;
 SELECT * FROM koristus;
-SELECT * FROM toit;
 
--- protseduur 1: lisa loom
+-- Protseduur: lisa loom
 CREATE PROCEDURE lisaLoom
-@liik VARCHAR(30),
-@tõug VARCHAR(30),
-@sugu CHAR(1),
-@kuup DATE,
+@nimi VARCHAR(50),
+@liik VARCHAR(50),
+@tõug VARCHAR(50),
+@sugu VARCHAR(10),
+@kuupäev DATE,
 @boks INT
 AS
 BEGIN
-    INSERT INTO loom(liik, tõug, sugu, saabumise_kuupäev, boks_id)
-    VALUES (@liik, @tõug, @sugu, @kuup, @boks);
-
+    INSERT INTO loom(nimi, liik, tõug, sugu, saabumise_kuupäev, boks_id)
+    VALUES (@nimi, @liik, @tõug, @sugu, @kuupäev, @boks);
     SELECT * FROM loom;
 END;
+GO
 
--- protseduur 2: otsi loomi soo järgi
-CREATE PROCEDURE otsiSugu
-@s CHAR(1)
+EXEC lisaLoom 'TestLoom', 'Koer', 'Segavereline', 'Isane', '2024-03-01', 1;
+GO
+
+-- Protseduur: otsi loom liigi järgi
+CREATE PROCEDURE otsiLoomLiigiJargi
+@liik VARCHAR(50)
 AS
 BEGIN
-    SELECT * FROM loom WHERE sugu = @s;
+    SELECT * FROM loom
+    WHERE liik = @liik;
 END;
+GO
 
--- protseduur 3: näita koristusi looma järgi
-CREATE PROCEDURE koristusedLoomaJargi
-@id INT
+EXEC otsiLoomLiigiJargi 'Koer';
+GO
+
+-- Protseduur: näita koristusi looma järgi
+CREATE PROCEDURE naitaKoristusi
+@loom INT
 AS
 BEGIN
-    SELECT k.koristus_id, k.kuupäev, k.kestus, v.nimi
+    SELECT k.koristus_id, l.nimi AS loom, v.nimi AS vabatahtlik, k.kuupäev, k.kestus
     FROM koristus k
+    JOIN loom l ON k.loom_id = l.loom_id
     JOIN vabatahtlik v ON k.vabatahtlik_id = v.vabatahtlik_id
-    WHERE k.loom_id = @id;
+    WHERE k.loom_id = @loom;
 END;
+GO
 
--- kutsed
-EXEC lisaLoom 'Koer', 'Mops', 'M', '2024-02-10', 1;
-EXEC otsiSugu 'N';
-EXEC koristusedLoomaJargi 2;
+EXEC naitaKoristusi 1;
+GO
