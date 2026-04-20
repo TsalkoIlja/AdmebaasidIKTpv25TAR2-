119f3b14450b48f93528e613866066202b768d5d
TsalkoIlja <iljatsalko9@gmail.com>
2026-04-20 10:17:12 +0300

    Add files via upload

---
+++ TriggerLogiProcedure.sql
@@ -0,0 +1,14 @@
+﻿
+
+INSERT INTO product(product_name, brand_id, category_id, model_year, list_price)
+VALUES
+('Mountain Bike X1', 1, 1, 2022, 899.00),
+('Road Bike Pro', 2, 1, 2023, 1299.00),
+('City Bike Comfort', 3, 2, 2021, 499.00),
+('Electric Bike E‑200', 4, 3, 2024, 1899.00),
+('Kids Bike Mini', 5, 4, 2020, 199.00),
+('Hybrid Bike H‑500', 2, 2, 2023, 799.00),
+('Test product 1', 1, 1, 2024, 599.00),
+('Test product 2', 1, 2, 2018, 599.00);
+
+Select * From product;
